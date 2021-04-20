c210ad4ac159c28c9bfa9fe10b55c7e9c81b7e71
ilia_solovev <ilia_solovev@epam.com>
2021-04-20 11:43:55 +0400

    Learning Spring - Lesson 14 - Java Configuration

--- ilia-solovev/spring-app1/spring-app1/src/main/java/com/solovev/spring/TestSpring.java
+++ ilia-solovev/spring-app1/spring-app1/src/main/java/com/solovev/spring/TestSpring.java
@@ -1,13 +1,17 @@
 package com.solovev.spring;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestSpring {
 
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-                "applicationContext.xml"
-        );
+        ClassPathXmlApplicationContext contextXML = new ClassPathXmlApplicationContext(
+                "applicationContext.xml"); //configuration in XML file
+
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
+                SpringConfig.class); //configuration in XML file
+
 
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 
